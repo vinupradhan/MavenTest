bf75c80dd722d20b83c914d1860cfb2a6d331bd1
vinay <vinay@Ramu-PV>
2022-03-15 14:36:36 +0530

    Committed By Vinay

---
+++ Maven1/src/test/java/ProductsTest/Demo.java
@@ -0,0 +1,19 @@
+package ProductsTest;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void demo()
+	{
+		System.out.println("Hello This is a Test");
+		
+		System.out.println("This code is final");
+		
+		
+		
+	}
+	
+	
+}
