800e0affa520c8789b59aa3b5a0b0ab9f05131bb
vinay <vinay@Ramu-PV>
2022-03-16 10:46:20 +0530

    Committed By VP

---
+++ Maven1/src/test/java/ProductsTest/ParameterisedTest.java
@@ -0,0 +1,12 @@
+package ProductsTest;
+
+import org.testng.annotations.Test;
+
+public class ParameterisedTest {
+
+	@Test
+	public void parameterisedTest()
+	{
+		System.out.println("browser");	
+	}
+}
